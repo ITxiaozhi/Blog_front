205c1490070e2b6e4cb11db348134f6954114640
ITxiaozhi <hanshouzhi204113@163.com>
2020-03-23 23:07:23 +0800

    添加通知和底部

--- src/components/container/SpeedBar.vue
+++ src/components/container/SpeedBar.vue
@@ -1,23 +1,34 @@
 <template>
-  <div class="speed">speedbar</div>
+  <div class="speed">
+    <i class="iconfont icontongzhi1"></i>
+    <div class="info">本站是最牛逼的网站</div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "SpeedBar"
-    }
+  export default {
+    name: "SpeedBar"
+  }
 </script>
 
 <style scoped>
-.speed{
-  color: #666;
-  /*position: absolute;*/
-  background-color: #fbfbfb;
-  padding: 8px 20px 18px;
-  line-height: 25px;
-  overflow: hidden;
-  height: 14px;
-  margin-top: 0px;
-  margin-bottom: 10px;
-}
+  .speed {
+    color: #666;
+    /*position: absolute;*/
+    background-color: #fbfbfb;
+    padding: 8px 20px 18px;
+    line-height: 25px;
+    overflow: hidden;
+    height: 14px;
+    margin-top: 0;
+    margin-bottom: 10px;
+    display: flex;
+    flex-direction: row;
+  }
+  .speed .icontongzhi1{
+    color: #56af45;
+  }
+  .speed .info{
+    margin-left: 5px;
+  }
 </style>
--- src/components/footer/Copyright.vue
+++ src/components/footer/Copyright.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="copyright"><a>footer</a></div>
+  <div class="copyright">
+    <a @click="jump('/')">IT-小志</a>|
+    <a href="http://www.beian.miit.gov.cn/" target="_blank">京ICP备20007546</a>
+  </div>
 </template>
 
 <script>
   export default {
-    name: "Copyright"
+    name: "Copyright",
+    methods: {
+      jump(route) {
+        this.$router.push(route);
+      },
+    }
   }
 </script>
 
